440279e82039e1f547a553128eb3bf14f0fa5a2a
Andy Gonzales <andres.gonzales.ext@aboutyou.com>
2022-08-30 14:45:55 +0700

    try again

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,41 @@
 set -e
 
 PHP_FULL_VERSION=$(php -r 'echo phpversion();')
+#
+#if [ -z "$1" ]; then
+#  ARGS="."
+#else
+#  ARGS="$1"
+#fi
+#
+#if [ -z "$2" ]; then
+#  EXCLUDE="."
+#else
+#  EXCLUDE="--exclude $2"
+#fi
 
-if [ -z "$1" ]; then
-  ARGS="."
-else
-  ARGS="$1"
-fi
+i=1;
+j=$#;
+CMD_STRING=""
+while [ $i -le $j ]
+  do
+    case $1 in
 
-if [ -z "$2" ]; then
-  EXCLUDE="."
-else
-  EXCLUDE="--exclude $2"
-fi
+      --exclude|-e)
+        shift
+        CMD_STRING=CMD_STRING+" --excluding '${1}'"
+        ;;
 
+      *)
+        CMD_STRING=CMD_STRING+" '${1}'"
+        ;;
+
+    esac
+    i=$((i + 1));
+    shift 1;
+  done
+
+echo "/phpcd $CMD_STRING"
 echo "PHP Version : ${PHP_FULL_VERSION}"
 echo "/phpcpd ${ARGS} $EXCLUDE"
 
