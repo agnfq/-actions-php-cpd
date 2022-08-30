e43dd4fdfe4da0bbd39470c89050b132a71854f2
Andy Gonzales <andres.gonzales.ext@aboutyou.com>
2022-08-30 11:47:54 +0700

    Initial try

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+PHP_FULL_VERSION=$(php -r 'echo phpversion();')
+
+if [ -z "$1" ]; then
+  ARGS="."
+else
+  ARGS="$1"
+fi
+
+echo "## Running PHP Copy Paste Detector with ${ARGS}"
+echo "PHP Version : ${PHP_FULL_VERSION}"
+
+/phpcpd "${ARGS}"
