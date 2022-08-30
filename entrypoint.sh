07619feeccd0a726e287dace0ff8eb47d3d01216
Andy Gonzales <andres.gonzales.ext@aboutyou.com>
2022-08-30 13:32:36 +0700

    try again

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,25 +3,7 @@ set -e
 
 PHP_FULL_VERSION=$(php -r 'echo phpversion();')
 
-case $1 in
-     --root|-r)
-         shift
-         echo "Root dir: $1"
-         ROOT="$1";
-         EXCLUDE="";
-         ;;
-     --exclude|-e)
-         shift
-         echo "Excluding: $1";
-         ROOT="./src";
-         EXCLUDE="--exclude $1";
-         shift
-         ;;
-     *)
-        ;;
-esac
-
 echo "## Running PHP Copy Paste Detector with ${ROOT} ${EXCLUDE}"
 echo "PHP Version : ${PHP_FULL_VERSION}"
 
-/phpcpd ${ROOT} ${EXCLUDE}
+/phpcpd ./src --exclude ./src/Modules/Shop/Domain/Entity
