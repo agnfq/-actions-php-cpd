5ba9dde03e0bd355375d75a205d4fdaf9d4afd01
Andy Gonzales <andres.gonzales.ext@aboutyou.com>
2022-08-30 11:56:25 +0700

    try again

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,10 @@ PHP_FULL_VERSION=$(php -r 'echo phpversion();')
 if [ -z "$1" ]; then
   ARGS="."
 else
-  ARGS="$1"
+  ARGS=$1
 fi
 
 echo "## Running PHP Copy Paste Detector with ${ARGS}"
 echo "PHP Version : ${PHP_FULL_VERSION}"
 
-/phpcpd "${ARGS}"
+/phpcpd ${ARGS}
